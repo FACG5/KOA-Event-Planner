1857e9739282e58b14264890a74391f024bfe03d
asmaa-ezz <you@example.com>
2018-08-07 14:55:58 +0300

    Add db_bulid Relates #8

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,7 +1,27 @@
-const {pool} = require ("pg");
-const url = require ("url");
-require ("env2")("./config.env");
+const {Pool} = require('pg');
+const url = require('url');
 
-if (!process.env.DB_URL) throw new Error ("DB_URL must be set");
-const params= url.parse(process.env.DB_URL);
-const [username, password]= params.auth.split(":");
+const env = require('env2');
+env('./config.env');
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('Environment variable DATABASE_URL must be set');
+}
+
+const params = url.parse(process.env.DATABASE_URL);
+
+const [username, password] = params.auth.split(':');
+
+const options = {
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    max: process.env.DB_MAX_CONNECTIONS || 2
+};
+
+if (username) { options.user = username; }
+if (password) { options.password = password; }
+
+options.ssl = (options.host !== 'localhost');
+
+module.exports = new Pool(options);
