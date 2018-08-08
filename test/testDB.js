8e554a8ba0e70ccdd1d9cf0ef43888220cbe2b19
asmaa-ezz <you@example.com>
2018-08-08 16:44:18 +0300

    Add testing

--- test/testDB.js
+++ test/testDB.js
@@ -1,9 +1,25 @@
 const tape = require("tape");
-// const runDbBuild = require("../database/db_build");
-// const getData = require("../src/queries/getData");
-// const postData = require("../src/queries/postData");
+const runDbBuild = require("../database/db_build");
+const { getData, addUser, addEvent, gitEvent, isUserName, searchUser } = require("../src/getData");
 
-tape("tape is working", t => {
-    t.equals(1, 1, "one equals one");
-    t.end();
+tape('Test of git Data', (t)=> {
+    runDbBuild(function(error, respons){
+        getData((err, res) =>{
+            if(err) t.error(err, "No Error");
+
+            t.equal(res.length>0,true, 'DB have row data');
+            t.end();
+        });
+    })
 });
+
+tape('Test of funcation add user', (t)=> {
+    runDbBuild(function(error, respons){
+        addUser('asmaaizz','111','asmaa',(err, res) =>{
+            if(err)  t.error(err, "No Error");
+            t.equal(res[0].user_name, 'asmaaizz' , 'user_name equal asmaaizz');
+            t.end();
+        });
+    })
+});
+
